fd994a783dbf63f7ce2e500fda1a87c0a0564d3c
kizikli27 <77758476+kizikli27@users.noreply.github.com>
2024-01-19 17:21:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  double x=1;
+      double carpim=1;
+  while(x<=44){
+      carpim = carpim* x;
+      x++;
+       
+  }
+  cout<<"ÇARPIM DEĞERİNİZ:"<<carpim<<endl;
+}
